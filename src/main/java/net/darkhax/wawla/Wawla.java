f7aac11ccba5ef5e3ddeec248140bd70f27614d5
Tyler Hancock <darklime@live.ca>
2016-06-28 05:37:02 -0600

    Switched to an OML mod

--- src/main/java/net/darkhax/wawla/Wawla.java
+++ src/main/java/net/darkhax/wawla/Wawla.java
@@ -1,88 +1,11 @@
 package net.darkhax.wawla;
 
-import java.util.ArrayList;
-import java.util.List;
+import xyz.openmodloader.modloader.Mod;
 
-import net.darkhax.icse.ICSE;
-import net.darkhax.wawla.common.CommonProxy;
-import net.darkhax.wawla.engine.ICSEEngine;
-import net.darkhax.wawla.engine.InfoEngine;
-import net.darkhax.wawla.engine.WailaEngine;
-import net.darkhax.wawla.lib.Constants;
-import net.darkhax.wawla.lib.WawlaConfiguration;
-import net.darkhax.wawla.plugins.InfoProvider;
-import net.darkhax.wawla.plugins.generic.entities.PluginAnimal;
-import net.darkhax.wawla.plugins.generic.entities.PluginArmorPoints;
-import net.darkhax.wawla.plugins.generic.entities.PluginEquipment;
-import net.darkhax.wawla.plugins.generic.entities.PluginTameable;
-import net.darkhax.wawla.plugins.generic.tiles.PluginBlastResistance;
-import net.darkhax.wawla.plugins.generic.tiles.PluginBreakProgression;
-import net.darkhax.wawla.plugins.generic.tiles.PluginEnchantmentPower;
-import net.darkhax.wawla.plugins.generic.tiles.PluginHardness;
-import net.darkhax.wawla.plugins.generic.tiles.PluginHarvestability;
-import net.darkhax.wawla.plugins.vanilla.entities.PluginEXPOrb;
-import net.darkhax.wawla.plugins.vanilla.entities.PluginHorse;
-import net.darkhax.wawla.plugins.vanilla.entities.PluginItemFrame;
-import net.darkhax.wawla.plugins.vanilla.entities.PluginPrimedTNT;
-import net.darkhax.wawla.plugins.vanilla.entities.PluginVillagerTypes;
-import net.darkhax.wawla.plugins.vanilla.tiles.PluginFurnace;
-import net.darkhax.wawla.plugins.vanilla.tiles.PluginSkulls;
-import net.minecraftforge.fml.common.Loader;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
-@Mod(modid = Constants.MODID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER, acceptableRemoteVersions = "*", dependencies = "after:ICSE")
-public class Wawla {
-    
-    @SidedProxy(serverSide = Constants.SERVER, clientSide = Constants.CLIENT)
-    public static CommonProxy proxy;
-    
-    @Mod.Instance(Constants.MODID)
-    public static Wawla instance;
-    
-    public static InfoEngine engine;
-    public static final List<InfoProvider> tileProviders = new ArrayList<InfoProvider>();
-    public static final List<InfoProvider> entityProviders = new ArrayList<InfoProvider>();
-    
-    @EventHandler
-    public void preInit (FMLPreInitializationEvent event) {
+public class Wawla implements Mod {
+	
+    @Override
+    public void onInitialize() {
         
-        if (Loader.isModLoaded("Waila"))
-            engine = new WailaEngine();
-            
-        else if (Loader.isModLoaded("ICSE")) {
-            
-            engine = new ICSEEngine();
-            ICSE.plugins.add((ICSEEngine) engine);
-        }
-        
-        // Generic Tiles
-        tileProviders.add(new PluginBreakProgression());
-        tileProviders.add(new PluginEnchantmentPower());
-        tileProviders.add(new PluginBlastResistance());
-        tileProviders.add(new PluginHardness());
-        tileProviders.add(new PluginHarvestability());
-        
-        // Generic Entities
-        entityProviders.add(new PluginEquipment());
-        entityProviders.add(new PluginArmorPoints());
-        entityProviders.add(new PluginTameable());
-        entityProviders.add(new PluginAnimal());
-        
-        // Vanilla tiles
-        tileProviders.add(new PluginSkulls());
-        tileProviders.add(new PluginFurnace());
-        
-        // Vanilla Entities
-        entityProviders.add(new PluginHorse());
-        entityProviders.add(new PluginVillagerTypes());
-        entityProviders.add(new PluginItemFrame());
-        entityProviders.add(new PluginPrimedTNT());
-        entityProviders.add(new PluginEXPOrb());
-        
-        new WawlaConfiguration(event.getSuggestedConfigurationFile());
-        proxy.preInit();
     }
 }
